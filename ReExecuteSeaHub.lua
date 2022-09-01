d712d1d0f73911cf91a246081fb3daef157791fa
AltsegoD <59056606+AltsegoD@users.noreply.github.com>
2022-09-01 15:54:17 +0700

    Update ReExecuteSeaHub.lua

--- ReExecuteSeaHub.lua
+++ ReExecuteSeaHub.lua
@@ -1,11 +1,12 @@
-if game:IsLoaded() then
+if game:IsLoaded()
+and game.PlaceId then
     game.StarterGui:SetCore("SendNotification", {
       Title = "Sea Hub",
       Text = "This game is not supported, make sure to check #features channel that you executed on the corrected game",
       Duration = 20
     })
 else
-    repeat wait() until game:IsLoaded() wait(2)
+    repeat wait() until game:IsLoaded() and game.PlaceId wait(2)
     warn("Attempted to Re-Execute Sea Hub")
     loadstring(game:HttpGet("https://ego-d.xyz/Script"))()
 end
